a24cc3301e97c880a5f81f57f43a2c417bf61c61
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-06-25 17:30:32 +0200

    added custom message into homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,17 @@
+import { useAuth } from "../contexts/AuthContext";
+
 const Home = () => {
+
+    const { user, isLoggedIn } = useAuth();
+
     return (
-        <h1>HomePage</h1>
+        <div>
+            {isLoggedIn ?
+                <h1>Benvenuto {user.name}!</h1>
+                :
+                <h1>Benvenuto!</h1>
+            }
+        </div>
     );
 }
 
